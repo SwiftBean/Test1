f85a5b4904c601862fd19701233262900914c0b7
SwiftBean <42588386+SwiftBean@users.noreply.github.com>
2018-09-07 14:13:52 -0600

    first push

---
+++ fa.py
@@ -0,0 +1,34 @@
+##print("hi class")
+##
+##first_name = "Zach"
+##
+##print(first_name)
+##
+##fact= "I love video games"
+##print("my name is" +first_name + "and" + fact)
+##message = first_name + fact
+##print(message)
+##
+##num1 = 1
+##num2 = 2
+##answer = num1 + num2 - num1 * num1
+##print(answer)
+
+
+##word1 = input("enter a name")
+##word2 = input("enter an animal")
+##word3 = input("enter a liquid") 
+##word4 = input("enter a substance") 
+##print(word1, "is sick with the", word2, "flu. Drink more", word3, "and take", word4, "as needed.")
+
+
+score1=input("score") 
+score2=input("score")
+score2=int(score2)
+score3=input("score")
+score4=input("score")
+score5=input("score")
+sum1=int(score1)+int(score2)+int(score3)+int(score4)+int(score5)
+avg=sum1/5
+print(avg)
+input() 
