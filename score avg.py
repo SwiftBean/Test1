3b24ea4da75e3aa0e26edd3df8b9a267b0f542ae
SwiftBean <42588386+SwiftBean@users.noreply.github.com>
2018-09-17 14:02:13 -0600

    use this to get the average of assignment or test scores

---
+++ score avg.py
@@ -0,0 +1,17 @@
+import math
+
+##def avg_score():
+score1=float(input("score"))
+score2=float(input("score"))
+score3=float(input("score"))
+score4=float(input("score"))
+score5=float(input("score"))
+score6=float(input("score"))
+score7=float(input("score"))
+score8=float(input("score"))
+score9=float(input("score"))
+score10=float(input("score"))
+sum1=score1+score2+score3+score4+score5+score6+score7+score8+score9+score10
+avg_score=sum1/10
+print(avg_score)
+input() 
